6f30110d26c8a86f580000b0fafc04ba78211202
seanluse41 <seanluse41@gmail.com>
2026-02-11 01:08:58 +0900

    sops

--- modules/ddns.nix
+++ modules/ddns.nix
@@ -1,20 +1,31 @@
 { config, ... }:
 {
-  services.ddns-updater = {
-    enable = true;
-    settings = {
-      settings = [
-        {
-          provider = "porkbun";
-          domain = "zoocom.homes";
-          host = "headscale";
-          api_key = config.sops.secrets.porkbun-api-key.path;
-          secret_api_key = config.sops.secrets.porkbun-secret-key.path;
-        }
-      ];
-    };
-  };
-
   sops.secrets.porkbun-api-key = {};
   sops.secrets.porkbun-secret-key = {};
+
+  sops.templates."ddns-config.json" = {
+    path = "/etc/ddns-updater/config.json";
+    content = ''
+      {
+        "settings": [
+          {
+            "provider": "porkbun",
+            "domain": "zoocom.homes",
+            "host": "headscale",
+            "api_key": "${config.sops.placeholder.porkbun-api-key}",
+            "secret_api_key": "${config.sops.placeholder.porkbun-secret-key}"
+          }
+        ]
+      }
+    '';
+  };
+
+  services.ddns-updater = {
+    enable = true;
+    environment = {
+      CONFIG_FILEPATH = "/etc/ddns-updater/config.json";
+      SERVER_ENABLED = "no";
+      PERIOD = "5m";
+    };
+  };
 }

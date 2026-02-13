611f50a7c6e661bfef945a876a9c0fc5e636994f
seanluse41 <seanluse41@gmail.com>
2026-02-13 12:43:46 +0900

    directory permissions

--- modules/immich.nix
+++ modules/immich.nix
@@ -4,15 +4,13 @@
     enable = true;
     host = "0.0.0.0";
     port = 2283;
-    mediaLocation = "/home/sean/immich-pictures";
+    mediaLocation = "/var/lib/immich-pictures";
     
-    # Database settings
     database = {
       enable = true;
       createDB = true;
     };
     
-    # Environment variables
     environment = {
       PUBLIC_LOGIN_PAGE_MESSAGE = "ルース家写真";
       LOG_LEVEL = "log";
